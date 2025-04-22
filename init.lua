7ec2528510334560e31c46f0b4b0c034e45061c4
Hayes <goteevreslajd@gmail.com>
2025-04-22 17:58:30 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -14,6 +14,9 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 vim.diagnostic.config({
   virtual_text = {
     prefix = '●', 
